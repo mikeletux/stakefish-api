dfe345a0c5ba19a7477bf05d77bff4d9d4e3b061
Miguel Sama <miguelsamamerino@gmail.com>
2023-02-09 15:56:17 +0100

    add models
    Signed-off-by: Miguel Sama <miguelsamamerino@gmail.com>

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -5,3 +5,22 @@ type UnixTime struct {
 	TimeStamp int64  `json:"date"`
 	Isk8s     bool   `json:"kubernetes"`
 }
+
+type Address struct { // Address and ValidateIPRequest can be shared as both are the same in terms of data.
+	Ip string `json:"ip"`
+}
+
+type Query struct {
+	Addresses []Address `json:"addresses"`
+	ClientIp  string    `json:"client_ip"`
+	CreatedAt int64     `json:"created_at"`
+	Domain    string    `json:"domain"`
+}
+
+type ValidateIPResponse struct {
+	Status bool `json:"status"`
+}
+
+type HTTPError struct {
+	Message string `json:"message"`
+}
