fde1d11348a973cb78acf6e0b271bad5a5077dab
Miguel Sama <miguelsamamerino@gmail.com>
2023-02-09 18:18:09 +0100

    add skeleton for handler tests
    Signed-off-by: Miguel Sama <miguelsamamerino@gmail.com>

---
+++ pkg/controller/handler_test.go
@@ -0,0 +1,15 @@
+package controller
+
+import "testing"
+
+// TO-DO
+
+func TestGetUnixTime(t *testing.T) {
+
+}
+
+func TestLookupDomain(t *testing.T) {}
+
+func TestValidateIP(t *testing.T) {}
+
+func TestRetrieveHistory(t *testing.T) {}
