0d03692e415b3581536d6651e87cb04289656a8e
Miguel Sama <miguelsamamerino@gmail.com>
2023-02-09 16:47:51 +0100

    add new handle func for validate ip
    Signed-off-by: Miguel Sama <miguelsamamerino@gmail.com>

--- pkg/controller/mux.go
+++ pkg/controller/mux.go
@@ -2,18 +2,25 @@ package controller
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/mikeletux/stakefish-api/pkg/database"
 	"net/http"
 )
 
-func NewController() http.Handler {
+type Manager struct {
+	Router http.Handler
+
+	dbConnector database.Connector
+}
+
+func NewController(dbConnector database.Connector) *Manager {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", GetUnixTime).Methods("GET")
 	//	router.HandleFunc("/metrics", TBDfunc).Methods("GET")
 	//	router.HandleFunc("/health", TBDfunc).Methods("POST")
-	//	router.HandleFunc("/v1/tools/lookup", TBDfunc).Methods("PUT")
-	//	router.HandleFunc("/v1/tools/validate", TBDfunc).Methods("DELETE")
-	//	router.HandleFunc("/v1/history", TBDfunc).Methods("DELETE")
+	// router.HandleFunc("/v1/tools/lookup", LookupDomain).Methods("GET")
+	router.HandleFunc("/v1/tools/validate", ValidateIP).Methods("POST")
+	// router.HandleFunc("/v1/history", RetrieveHistory).Methods("GET")
 
-	return router
+	return &Manager{Router: router, dbConnector: dbConnector}
 }
