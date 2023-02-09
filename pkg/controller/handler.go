d75b7d7781d9d22aaf26e8c72d0040b0aa63fddb
Miguel Sama <miguelsamamerino@gmail.com>
2023-02-09 16:47:27 +0100

    add handler for validate ip
    Signed-off-by: Miguel Sama <miguelsamamerino@gmail.com>

--- pkg/controller/handler.go
+++ pkg/controller/handler.go
@@ -7,6 +7,9 @@ import (
 	"time"
 )
 
+// GetUnixTime handles queries coming to "/" endpoint.
+// Returns a JSON object with the API version, UNIX timestamp and if the API is running in k8s.
+// TO-DO get app version better and figure out if the app is running in k8s using env vars.
 func GetUnixTime(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
@@ -17,4 +20,28 @@ func GetUnixTime(w http.ResponseWriter, r *http.Request) {
 	unixTime.Isk8s = false
 
 	json.NewEncoder(w).Encode(unixTime)
+	w.WriteHeader(http.StatusOK)
+}
+
+func LookupDomain(w http.ResponseWriter, r *http.Request) {
+	// TO-DO
+}
+
+func ValidateIP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var ipAddr models.Address
+	err := json.NewDecoder(r.Body).Decode(&ipAddr)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(models.HTTPError{Message: "Error decoding incoming body"})
+		return
+	}
+
+	result := checkIfStringIsIP(ipAddr.Ip) // TO-DO
+	json.NewEncoder(w).Encode(models.ValidateIPResponse{Status: result})
+}
+
+func RetrieveHistory(w http.ResponseWriter, r *http.Request) {
+	// TO-DO
 }
