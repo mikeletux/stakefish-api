e8935724dd9aa7ae62a9d4e674379f8a68866398
Miguel Sama <miguelsamamerino@gmail.com>
2023-02-09 17:46:44 +0100

    add handler for LookupDomain and fix small nit
    Signed-off-by: Miguel Sama <miguelsamamerino@gmail.com>

--- pkg/controller/handler.go
+++ pkg/controller/handler.go
@@ -2,15 +2,17 @@ package controller
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/mikeletux/stakefish-api/pkg/models"
 	"net/http"
+	"strings"
 	"time"
 )
 
 // GetUnixTime handles queries coming to "/" endpoint.
 // Returns a JSON object with the API version, UNIX timestamp and if the API is running in k8s.
 // TO-DO get app version better and figure out if the app is running in k8s using env vars.
-func GetUnixTime(w http.ResponseWriter, r *http.Request) {
+func (m *Manager) GetUnixTime(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	var unixTime models.UnixTime
@@ -20,14 +22,36 @@ func GetUnixTime(w http.ResponseWriter, r *http.Request) {
 	unixTime.Isk8s = false
 
 	json.NewEncoder(w).Encode(unixTime)
-	w.WriteHeader(http.StatusOK)
 }
 
-func LookupDomain(w http.ResponseWriter, r *http.Request) {
-	// TO-DO
+func (m *Manager) LookupDomain(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	domain := r.URL.Query().Get("domain")
+	if len(strings.TrimSpace(domain)) == 0 { // If no domain is provided, inform client.
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(models.HTTPError{Message: "No domain provided"})
+		return
+	}
+
+	ips, err := lookupIPv4Addr(domain)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(models.HTTPError{Message: "No domain provided"})
+		return
+	}
+
+	if len(ips) == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(models.HTTPError{Message: fmt.Sprintf("No IPv4 are associated with %s", domain)})
+		return
+	}
+
+	domainLookup := retrieveDomainLookup(ips, domain)
+	json.NewEncoder(w).Encode(domainLookup)
 }
 
-func ValidateIP(w http.ResponseWriter, r *http.Request) {
+func (m *Manager) ValidateIP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	var ipAddr models.Address
@@ -42,6 +66,6 @@ func ValidateIP(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(models.ValidateIPResponse{Status: result})
 }
 
-func RetrieveHistory(w http.ResponseWriter, r *http.Request) {
+func (m *Manager) RetrieveHistory(w http.ResponseWriter, r *http.Request) {
 	// TO-DO
 }
