3706fafd6d1915101a43a3520594b2a6fdb9a241
Miguel Sama <miguelsamamerino@gmail.com>
2023-02-09 17:47:48 +0100

    add support functions for domain lookup
    Signed-off-by: Miguel Sama <miguelsamamerino@gmail.com>

--- pkg/controller/util.go
+++ pkg/controller/util.go
@@ -1,10 +1,41 @@
 package controller
 
-import regexp2 "regexp"
+import (
+	"github.com/mikeletux/stakefish-api/pkg/models"
+	"net"
+	"regexp"
+	"time"
+)
 
 const ipRegExp string = `^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$`
 
 func checkIfStringIsIP(ip string) bool {
-	regexp := regexp2.MustCompile(ipRegExp)
+	regexp := regexp.MustCompile(ipRegExp)
 	return regexp.MatchString(ip)
+}
+
+func lookupIPv4Addr(domain string) ([]models.Address, error) {
+	var ipv4 []models.Address
+	ips, err := net.LookupIP(domain)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, ip := range ips {
+		if ip.To4() != nil { // This means ip struct is an IPv4
+			ipv4 = append(ipv4, models.Address{Ip: ip.String()})
+		}
+	}
+
+	return ipv4, nil
+}
+
+func retrieveDomainLookup(addresses []models.Address, domain string) models.Query {
+	return models.Query{
+		Addresses: addresses,
+		ClientIp:  "192.168.1.2", // Change this :)
+		CreatedAt: time.Now().Unix(),
+		Domain:    domain,
+	}
 }
