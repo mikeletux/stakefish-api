c04cea3b13519cbd9cca005a2cd563e35516ee7e
Miguel Sama <miguelsamamerino@gmail.com>
2023-02-10 16:57:27 +0100

    update db interface and implementation
    Signed-off-by: Miguel Sama <miguelsamamerino@gmail.com>

--- pkg/infra/db.go
+++ pkg/infra/db.go
@@ -1,30 +1,55 @@
 package infra
 
+import (
+	"context"
+	"github.com/go-pg/pg/v10"
+	"github.com/mikeletux/stakefish-api/pkg/models"
+)
+
 type DBConnector interface {
-	Connect() error
-	MakeQuery(query string) error
+	Ping() error
+	SaveQuery(query models.Query) error
+	RetrieveLastTwentyQueries() ([]models.Query, error)
 	Close() error
 }
 
 type PostgresConnector struct {
-	// TO-DO
+	db *pg.DB
 }
 
 func NewPostgresConnector() *PostgresConnector {
-	return &PostgresConnector{}
+	db := pg.Connect(&pg.Options{
+		Addr:     ":5432",
+		User:     "postgres",
+		Password: "postgres",
+		Database: "stakefish",
+	})
+
+	return &PostgresConnector{db: db}
 }
 
-func (p *PostgresConnector) Connect() error {
-	// TO-DO
+func (p *PostgresConnector) Ping() error {
+	ctx := context.Background() // We should be handling here timeouts.
+	return p.db.Ping(ctx)
+}
+
+func (p *PostgresConnector) SaveQuery(query models.Query) error {
+	_, err := p.db.Model(&query).Insert()
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-func (p *PostgresConnector) MakeQuery(query string) error {
-	// TO-DO
-	return nil
+func (p *PostgresConnector) RetrieveLastTwentyQueries() ([]models.Query, error) {
+	var queries []models.Query
+	err := p.db.Model(&queries).Order("CreatedAt DESC").Limit(20).Select()
+	if err != nil {
+		return nil, err
+	}
+	return queries, nil
 }
 
 func (p *PostgresConnector) Close() error {
-	// TO-DO
-	return nil
+	return p.db.Close()
 }
