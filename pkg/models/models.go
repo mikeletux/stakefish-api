8b872935eda489b9682346643daf1f532d24bb72
Miguel Sama <miguelsamamerino@gmail.com>
2023-02-10 17:25:27 +0100

    update models
    Signed-off-by: Miguel Sama <miguelsamamerino@gmail.com>

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -7,17 +7,19 @@ type UnixTime struct {
 }
 
 type Address struct { // Address and ValidateIPRequest can be shared as both are the same in terms of data.
-	Id int64  `json:"-" pg:",default,pk"`
-	Ip string `json:"ip"`
+	tableName struct{} `json:"-" pg:"address"`
+	Id        int64    `json:"-" pg:"id,default,pk"`
+	Ip        string   `json:"ip" pg:"ip"`
+	QueryID   int64    `json:"-" pg:"query_id"`
 }
 
 type Query struct {
-	tableName struct{} `json:"-" pg:"query"`
-	Id        int64    `json:"-" pg:"id,default,pk"`
-	// Addresses []Address `json:"addresses" pg:"rel:has-many"`
-	ClientIp  string `json:"client_ip" pg:"client_ip"`
-	CreatedAt int64  `json:"created_at" pg:"created_at"`
-	Domain    string `json:"domain" pg:"domain"`
+	tableName struct{}  `json:"-" pg:"query"`
+	Id        int64     `json:"-" pg:"id,default,pk"`
+	Addresses []Address `json:"addresses" pg:"rel:has-many"`
+	ClientIp  string    `json:"client_ip" pg:"client_ip"`
+	CreatedAt int64     `json:"created_at" pg:"created_at"`
+	Domain    string    `json:"domain" pg:"domain"`
 }
 
 type ValidateIPResponse struct {
