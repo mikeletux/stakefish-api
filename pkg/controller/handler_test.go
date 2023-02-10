191a46c2508f0938974b97a89872bbc57f58a086
Miguel Sama <miguelsamamerino@gmail.com>
2023-02-10 19:51:13 +0100

    add TestValidateIP test
    Signed-off-by: Miguel Sama <miguelsamamerino@gmail.com>

--- pkg/controller/handler_test.go
+++ pkg/controller/handler_test.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"bytes"
 	"encoding/json"
 	"github.com/mikeletux/stakefish-api/pkg/infra"
 	"github.com/mikeletux/stakefish-api/pkg/models"
@@ -47,6 +48,56 @@ func TestGetUnixTime(t *testing.T) {
 
 func TestLookupDomain(t *testing.T) {}
 
-func TestValidateIP(t *testing.T) {}
+func TestValidateIP(t *testing.T) {
+	testTable := []struct {
+		ipAddr string
+		isIp   bool
+	}{
+		{
+			ipAddr: "192.168.10.1",
+			isIp:   true,
+		},
+		{
+			ipAddr: "256.500.1.3",
+			isIp:   false,
+		},
+		{
+			ipAddr: "15.76.45.1",
+			isIp:   true,
+		},
+		{
+			ipAddr: "0.300.1.2",
+			isIp:   false,
+		},
+	}
+
+	db := infra.NewMockConnector()
+	mockInfra := infra.MockInfra{}
+
+	manager := NewController(db, mockInfra)
+
+	for _, test := range testTable {
+		requestBody, err := json.Marshal(models.Address{Ip: test.ipAddr})
+		if err != nil {
+			t.Errorf("error when marshalling struct models.Address to byte")
+		}
+
+		r, _ := http.NewRequest("POST", "/v1/tools/validate", bytes.NewReader(requestBody))
+		w := httptest.NewRecorder()
+
+		manager.ValidateIP(w, r)
+
+		var validateIp models.ValidateIPResponse
+		err = json.NewDecoder(w.Body).Decode(&validateIp)
+		if err != nil {
+			t.Errorf("error when decoding JSON from response")
+		}
+
+		if validateIp.Status != test.isIp {
+			t.Errorf("ip check for %s is %t when it was supposed to be %t", test.ipAddr, validateIp.Status, test.isIp)
+		}
+	}
+
+}
 
 func TestRetrieveHistory(t *testing.T) {}
